7eb3aaf72dab292c312f961e20b150316ae6f3a5
sarthak18593 <anandsarthak@gmail.com>
2017-01-21 17:45:38 -0500

    Have Fun

--- Ninja_DB.py
+++ Ninja_DB.py
@@ -1,38 +1,18 @@
 
-# coding: utf-8
-
-# In[1]:
-
 import pymongo
+from PIL import Image, ImageDraw
 
-
-# In[2]:
 
 from pymongo import MongoClient
 client = MongoClient()
 
-
-# In[3]:
-
 client = MongoClient('mongodb://localhost:27017/')
-
-
-# In[4]:
 
 db = client.test_database
 
-
-# In[5]:
-
 collection = db.test_collection
 
-
-# In[6]:
-
 posts = db.posts
-
-
-# In[7]:
 
 post = [{"item": "Coconut Milk","section": "Canned & Packaged Foods", "X":4, "Y":1 ,"Aisle":"A","Shelf":"1"},
         {"item": "Baguette/French Bread","section": "Bakery, Breakfast, Cereal", "X":2, "Y":2 ,"Aisle":"B","Shelf":"3"},
@@ -46,27 +26,15 @@ post = [{"item": "Coconut Milk","section": "Canned & Packaged Foods", "X":4, "Y"
         {"item": "White Bread/Toast, enriched","section": "Bakery, Breakfast, Cereal", "X":3, "Y":2 ,"Aisle":"B","Shelf":"2"}]
 
 
-# In[8]:
-
 result= posts.insert_many(post)
-
-
-# In[9]:
 
 import pprint
 
 
-# In[10]:
-
 pprint.pprint(posts.find_one({"item": "Yellow Mustard"}))
 
 
-# In[11]:
-
 posts.find_one({"item": "Coconut Milk"})["X"]
-
-
-# In[12]:
 
 def get_coordinates (item_names):
     x_list = []
@@ -79,12 +47,8 @@ def get_coordinates (item_names):
     return my_list
 
 
-# In[13]:
-
 get_coordinates(["Coconut Milk", "Yellow Mustard"])
 
-
-# In[29]:
 
 def path_define (x1,x2):
     if ( min((x1+x2-2),(10-x1-x2))==(x1+x2-2)): #take left path
@@ -95,26 +59,36 @@ def path_define (x1,x2):
 def distance (p1,p2):
     x1,y1=p1
     x2,y2=p2
+    xd1 = x1*50
+    xd2= x2*50
+    yd1= y1*50
+    yd2= y2*50
+    im = Image.open('GroceryStoreFinal.jpg')#Draw line
+    draw = ImageDraw.Draw(im)
     if (y1==y2): #horizental motion
         dist= abs(x1-x2)
+        draw.line((xd1,yd1, xd2,yd2), fill=100,width=10)
+        im.show()
     elif (x1==x2 and (x1==1 or x1==5) ): # vertical motion
         #this code could be optimized to consider case for a bigger number but for simiplicty we consider the base case only
         dist = abs(y1-y2)
+        draw.line((xd1,yd1, xd2,yd2), fill=100,width=10)
+        im.show()
     elif (path_define(x1,x2)==1):#left path
         dist = (x1+x2-2)+ abs (y1-y2)
+        draw.line((xd1,yd1, xd2,yd2), fill=100,width=10)
+        im.show()
     elif (path_define(x1,x2)==0): #right path
         dist = (10-x1-x2)+ abs (y1-y2)
+        draw.line((xd1,yd1, xd2,yd2), fill=100,width=10)
+        im.show()
     return dist
 
 
-# In[ ]:
 
-
-
-
-# In[95]:
 
 def shortest_path (mylist):
+
     if (len(mylist)==1):
         return mylist
     distlist=[]
@@ -127,22 +101,11 @@ def shortest_path (mylist):
             x=d
             mindist=distlist[d][1]
     mylist[1], mylist[x+1]=distlist[x][0],mylist[1]
+
     return mylist[0:1]+shortest_path(mylist[1:])
-
-
-# In[96]:
-
 print(shortest_path([(5,1),(3,3),(4,1),(1,2)]))
 
-
-# In[84]:
 
 list=[1,2,3,4]
 print(list[2:3])
 print(list+list[2:3])
-
-
-# In[ ]:
-
-
-
